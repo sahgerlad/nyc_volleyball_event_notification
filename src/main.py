0bdad62a27b360688bcaddb8969674800a4a35b2
sahgerlad <lad.sahger@gmail.com>
2025-02-24 09:28:55 -0500

    Fix logger to generate new log files daily

--- src/main.py
+++ src/main.py
@@ -21,16 +21,18 @@ def create_logger(path_log: str, logger_name: str = None):
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
 
-    if not logger.hasHandlers():
-        file_handler = logging.FileHandler(path_log)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+    if logger.hasHandlers():
+        logger.handlers.clear()
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.INFO)
-        stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
+    file_handler = logging.FileHandler(path_log)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
     return logger
 
 
